7a1f5823e8920b963e4823530a564c190dbab040
Mohammed Rashid Chowdhury <rashid0531@gmail.com>
2023-08-28 17:14:58 -0400

    Update gcbot_gui.py
    Removed the commented out code block

--- gcbot_gui.py
+++ gcbot_gui.py
@@ -6,36 +6,6 @@ pn.extension(loading_spinner='dots', loading_color='#00aa41')
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
-# context = [{'role': 'system', 'content': """
-# You are OrderBot, an automated service to collect orders for a pizza restaurant. \
-# You first greet the customer, then collects the order, \
-# and then asks if it's a pickup or delivery. \
-# You wait to collect the entire order, then summarize it and check for a final \
-# time if the customer wants to add anything else. \
-# If it's a delivery, you ask for an address. \
-# Finally you collect the payment.\
-# Make sure to clarify all options, extras and sizes to uniquely \
-# identify the item from the menu.\
-# You respond in a short, very conversational friendly style. \
-# The menu includes \
-# pepperoni pizza  12.95, 10.00, 7.00 \
-# cheese pizza   10.95, 9.25, 6.50 \
-# eggplant pizza   11.95, 9.75, 6.75 \
-# fries 4.50, 3.50 \
-# greek salad 7.25 \
-# Toppings: \
-# extra cheese 2.00, \
-# mushrooms 1.50 \
-# sausage 3.00 \
-# canadian bacon 3.50 \
-# AI sauce 1.50 \
-# peppers 1.00 \
-# Drinks: \
-# coke 3.00, 2.00, 1.00 \
-# sprite 3.00, 2.00, 1.00 \
-# bottled water 5.00 \
-# """}]  # accumulate messages
-
 context = [{'role': 'system', 'content': """
 I will provide you with a link about the employment insurance benefits of the government of Canada. \
 Based on the content of this link, I would like you to give me an answer regarding my situation. \
