5f8fdd41978649cac8554af2d008e445a38782bf
Mohammed Rashid Chowdhury <rashid0531@gmail.com>
2023-08-29 13:17:39 -0400

    Updated the UI to position the textbox in the bottom of the screen. The panel containing the conversation is also made scrollable.

--- gcbot_gui.py
+++ gcbot_gui.py
@@ -6,36 +6,6 @@ pn.extension(loading_spinner='dots', loading_color='#00aa41')
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
-# context = [{'role': 'system', 'content': """
-# You are OrderBot, an automated service to collect orders for a pizza restaurant. \
-# You first greet the customer, then collects the order, \
-# and then asks if it's a pickup or delivery. \
-# You wait to collect the entire order, then summarize it and check for a final \
-# time if the customer wants to add anything else. \
-# If it's a delivery, you ask for an address. \
-# Finally you collect the payment.\
-# Make sure to clarify all options, extras and sizes to uniquely \
-# identify the item from the menu.\
-# You respond in a short, very conversational friendly style. \
-# The menu includes \
-# pepperoni pizza  12.95, 10.00, 7.00 \
-# cheese pizza   10.95, 9.25, 6.50 \
-# eggplant pizza   11.95, 9.75, 6.75 \
-# fries 4.50, 3.50 \
-# greek salad 7.25 \
-# Toppings: \
-# extra cheese 2.00, \
-# mushrooms 1.50 \
-# sausage 3.00 \
-# canadian bacon 3.50 \
-# AI sauce 1.50 \
-# peppers 1.00 \
-# Drinks: \
-# coke 3.00, 2.00, 1.00 \
-# sprite 3.00, 2.00, 1.00 \
-# bottled water 5.00 \
-# """}]  # accumulate messages
-
 context = [{'role': 'system', 'content': """
 I will provide you with a link about the employment insurance benefits of the government of Canada. \
 Based on the content of this link, I would like you to give me an answer regarding my situation. \
@@ -53,7 +23,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0.1):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
@@ -79,10 +49,22 @@ button_conversation = pn.widgets.Button(name="Chat!")
 panels = []  # collect display
 
 interactive_conversation = pn.bind(collect_messages, button_conversation)
+# dashboard = pn.Column(
+#     pn.panel(interactive_conversation, loading_indicator=True, height=300),
+#     pn.Row(inp, button_conversation),
+#     scroll=True,
+#     height=950,
+#     width=950 
+# )
+
 dashboard = pn.Column(
-    inp,
-    pn.Row(button_conversation),
-    pn.panel(interactive_conversation, loading_indicator=True, height=300),
+                pn.Row(
+                    pn.panel(interactive_conversation, loading_indicator=True, height=300),
+                    scroll=True,
+                    height=850,
+                    width=850 
+                    ),
+                pn.Row(inp, button_conversation)
 )
 dashboard.servable()
 
